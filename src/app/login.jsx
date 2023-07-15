6634eff9e059f6516a10f2d2e31d0ea89f3a4a9a
SamanthaKelly01 <samanthak2002@hotmail.com>
2023-07-15 14:23:52 -0300

    Header

--- src/app/login.jsx
+++ src/app/login.jsx
@@ -1,8 +1,10 @@
 import React from "react";
+import Header from "../site/componentes/header";
 
 function login() {
   return (
     <div>
+      <Header />
       <h1> "aaaaa"</h1>
     </div>
   );
--- src/site/componentes/header.jsx
+++ src/site/componentes/header.jsx
@@ -2,55 +2,54 @@ import React from "react";
 
 function Header() {
   return (
-    <div>
-      <nav class="navbar navbar-expand-lg bg-body-tertiary">
-        <div class="container-fluid">
-          <a class="navbar-brand" href="/app">
-            99CRM
-          </a>
-          <button
-            class="navbar-toggler"
-            type="button"
-            data-bs-toggle="collapse"
-            data-bs-target="#navbarNav"
-            aria-controls="navbarNav"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-          >
-            <span class="navbar-toggler-icon"></span>
-          </button>
-          <div class="collapse navbar-collapse" id="navbarNav">
-            <ul class="navbar-nav">
-              <li class="nav-item">
-                <a class="nav-link active" aria-current="page" href="/app">
-                  Home
-                </a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="/app">
-                  Features
-                </a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="/app">
-                  Clientes
-                </a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="/app">
-                  Planos e Preços
-                </a>
-              </li>{" "}
-              <li class="nav-item">
-                <a class="nav-link" href="/app">
-                  Contato
-                </a>
-              </li>
-            </ul>
-          </div>
+    <nav className="navbar navbar-expand-md d-flex " data-bs-theme="dark">
+      <div className="container">
+        <div>
+        <a className="navbar-brand" href="/app">
+          <img src="Imagens/logo.png" alt="" height="28" />
+        </a>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button></div>
+        <div className="collapse navbar-collapse " id="navbarNav">
+          <ul className="navbar-nav  ">
+            <li className="nav-item">
+              <a className="nav-link active" aria-current="page" href="/app">
+                Home
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="/app">
+                Features
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="/app">
+                Clientes
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="/app">
+                Planos e Preços
+              </a>
+            </li>{" "}
+            <li className="nav-item">
+              <a className="nav-link" href="/app">
+                Contato
+              </a>
+            </li>
+          </ul>
         </div>
-      </nav>
-    </div>
+      </div>
+    </nav>
   );
 }
 
